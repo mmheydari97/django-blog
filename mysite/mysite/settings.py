9be288c8384f6b6985dffb49d0eb3400ddf9034d
mmheydari97 <heidarymm@yahoo.com>
2019-08-29 07:19:53 +0000

    deployed

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '2h$)f89vni@#gs0@)4!tt)@e%%pnyku9w6c#t6o@czyntcw^3a'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["mmheydari97.pythonanywhere.com"]
 
 
 # Application definition
